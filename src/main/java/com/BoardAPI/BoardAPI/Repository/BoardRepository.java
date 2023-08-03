f16d98982aca8652c23d0af3be6d0b9e34804fc0
ShaimaAlabri <shaima96alabri@hotmail.com>
2023-08-03 16:49:27 +0400

    updating BoardRepsitory interface

--- src/main/java/com/BoardAPI/BoardAPI/Repository/BoardRepository.java
+++ src/main/java/com/BoardAPI/BoardAPI/Repository/BoardRepository.java
@@ -1,4 +1,9 @@
 package com.BoardAPI.BoardAPI.Repository;
 
-public interface BoardRepository {
+import com.BoardAPI.BoardAPI.Models.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board,Long> {
 }
