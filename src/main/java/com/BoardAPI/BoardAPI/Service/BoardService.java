6351cde12f4a31f7afe107feca0288cd4e3c3417
ShaimaAlabri <shaima96alabri@hotmail.com>
2023-08-03 18:33:25 +0400

    Adding creat board code in BoardService

--- src/main/java/com/BoardAPI/BoardAPI/Service/BoardService.java
+++ src/main/java/com/BoardAPI/BoardAPI/Service/BoardService.java
@@ -1,8 +1,16 @@
 package com.BoardAPI.BoardAPI.Service;
 
+import com.BoardAPI.BoardAPI.Models.Board;
+import com.BoardAPI.BoardAPI.Repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BoardService {
-
+  @Autowired
+    BoardRepository boardRepository;
+//Create board
+    public Board createBoard (Board board){
+        return boardRepository.save(board);
+    }
 }
