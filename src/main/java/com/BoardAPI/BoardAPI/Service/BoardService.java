7101135ec15c865c604ed06db630b9e38989724e
ShaimaAlabri <shaima96alabri@hotmail.com>
2023-08-03 18:38:08 +0400

    adding get all boards code in BoardService

--- src/main/java/com/BoardAPI/BoardAPI/Service/BoardService.java
+++ src/main/java/com/BoardAPI/BoardAPI/Service/BoardService.java
@@ -5,6 +5,8 @@ import com.BoardAPI.BoardAPI.Repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
   @Autowired
@@ -12,5 +14,9 @@ public class BoardService {
 //Create board
     public Board createBoard (Board board){
         return boardRepository.save(board);
+    }
+//    get all boards
+    public List<Board> getBoard(){
+        return boardRepository.findAll();
     }
 }
